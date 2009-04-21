f8416cf0ecb02f5f55e4523ec61a7b058590d15d
Matthew Dempsky <matthew@dempsky.org>
2009-04-21 03:35:57 -0700

    add functions for using streamlined format packets; extend nonce to 12 bytes

--- tools/dnscurve.py
+++ tools/dnscurve.py
@@ -10,8 +10,31 @@ def dnscurve_getpubkey(name):
   return None
 
 
+def dnscurve_encode_streamlined_query(nonce, box, pubkey):
+  if len(nonce) != 12:
+    raise ValueError('Invalid nonce')
+  if len(pubkey) != 32 or ord(pubkey[31]) >= 128:
+    raise ValueError('Invalid public key')
+  return 'Q6fnvWj8' + pubkey + nonce + box
+
+def dnscurve_decode_streamlined_query(packet):
+  if len(packet) < 52 or packet[:8] != 'Q6fnvWj8':
+    raise ValueError('Not a streamlined query')
+  return (len[8:20], len[20:52], len[52:])
+
+def dnscurve_encode_streamlined_response(nonce, box):
+  if len(nonce) != 24:
+    raise ValueError('Invalid nonce')
+  return 'R6fnvWj8' + nonce + box
+
+def dnscurve_decode_streamlined_response(packet):
+  if len(packet) < 32 or packet[:8] != 'R6fnvWj8':
+    raise ValueError('Not a streamlined query')
+  return (len[8:32], len[32:])
+
+
 def dnscurve_encode_queryname(nonce, box, pubkey, zone):
-  if len(nonce) != 8:
+  if len(nonce) != 12:
     raise ValueError('Invalid nonce')
   if len(pubkey) != 32 or ord(pubkey[31]) >= 128:
     raise ValueError('Invalid public key')
@@ -35,9 +58,9 @@ def dnscurve_decode_queryname(name):
 
   key = base32.decode(s[3:] + '0')
   r = base32.decode(''.join(output))
-  if r < 8: raise ValueError('Not a DNSCurve query')
+  if r < 12: raise ValueError('Not a DNSCurve query')
 
-  return (key, r[:8], r[8:])
+  return (key, r[:12], r[12:])
 
 
 def dnscurve_encode_rdata(nonce, box):
@@ -48,7 +71,7 @@ def dnscurve_encode_rdata(nonce, box):
 def dnscurve_decode_rdata(rdata):
   data = ''.join(rdata)
   if len(data) < 8: raise ValueError('Invalid DNSCurve response')
-  return (data[:8], data[8:])
+  return (data[:12], data[12:])
 
 
 # Split 's' into a list of strings with length no greater than 'n'.
